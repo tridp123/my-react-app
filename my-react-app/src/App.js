79f917c51e87e075f8f7255b0474f121387d2107
Trịnh Nhất Trí <>
2026-03-21 12:23:41 +0700

    chore: add core React, React Router, and related dependencies.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,5 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import EnglishLearningPage from './EnglishLearningPage';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Chào mừng đến với ứng dụng học tiếng Anh!</p>
+        <Link className="App-link" to="/learn">
+          Bắt đầu học
+        </Link>
+      </header>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/learn" element={<EnglishLearningPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import EnglishLearningPage from './EnglishLearningPage';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Chào mừng đến với ứng dụng học tiếng Anh!</p>
+        <Link className="App-link" to="/learn">
+          Bắt đầu học
+        </Link>
+      </header>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <option path="/" element={<Home />} />
+        <option path="/learn" element={<EnglishLearningPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
 
 function App() {
   return (
